139263a7a24b679bd8060ca5add18189571a2ee1
lwz <905328134@qq.com>
2026-03-21 17:52:44 +0800

    add lora ft

--- download.py
+++ download.py
@@ -1,10 +1,11 @@
+from pathlib import Path
 from huggingface_hub import snapshot_download
 
 WORKERS = 16
-BASE_DIR = "/ppio_net0/code/download"
+BASE_DIR = Path("/ppio_net0/code/download")
 
 
-def download_model(repo_id: str, base_dir: str = BASE_DIR) -> str:
+def download_model(repo_id: str, base_dir: Path = BASE_DIR) -> str:
     # Save to: <base_dir>/<repo_name>
     local_dir = base_dir / repo_id.rsplit("/", 1)[-1]
     local_dir.mkdir(parents=True, exist_ok=True)
@@ -20,6 +21,6 @@ if __name__ == "__main__":
     for repo_id in [
         # "Qwen/Qwen3-VL-2B-Instruct",
         # "llava-hf/llava-onevision-qwen2-0.5b-ov-hf",
-        "llava-hf/llava-1.5-7b-hf"
+        "llava-hf/llava-1.5-7b-hf",
     ]:
         download_model(repo_id)
